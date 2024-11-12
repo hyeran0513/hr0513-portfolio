cdd430107150161b9226f19b760bb45c2f049490
hyeran0513 <hr_0513@naver.com>
2024-11-12 15:11:37 +0900

    배포 준비

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/hr0513-portfolio" : "",
+  assetPrefix: process.env.ASSET_PREFIX || "",
   env: {
-    ASSET_PREFIX: process.env.NODE_ENV === "production" ? "/hr0513-portfolio" : "",
+    ASSET_PREFIX: process.env.ASSET_PREFIX || "",
   },
 };
- 
+
 module.exports = nextConfig;
