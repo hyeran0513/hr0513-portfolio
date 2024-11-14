edbc5083c8964da4240dfe5f81e35c99198ab19d
hyeran0513 <hr_0513@naver.com>
2024-11-14 15:15:18 +0900

    반응형 작업 1차

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,93 +9,218 @@ import {
   projectData,
   stackData,
 } from "./data";
-import { FcFolder, FcCloseUpMode, FcBusinessContact, FcSupport, FcSportsMode } from "react-icons/fc";
+import {
+  FcFolder,
+  FcCloseUpMode,
+  FcBusinessContact,
+  FcSupport,
+  FcSportsMode,
+} from "react-icons/fc";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const About = () => {
   return (
-    <div className={styles.page}>
-      <div className={styles.info}>
-        {profileData.length > 0 && (
-          <div className={styles.infoBox}>
-            <p className={styles.title}><FcBusinessContact />PROFILE</p>
+    <>
+      <div className={styles.gridContainer}>
+        <div className={styles.info}>
+          {profileData.length > 0 && (
+            <div className={styles.infoBox}>
+              <p className={styles.title}>
+                <FcBusinessContact />
+                PROFILE
+              </p>
 
-            <div className={styles.infoList}>
-              {profileData.map((item, index) => (
-                <dl className={styles.infoItem} key={index}>
-                  <dt>{item.txt}</dt>
-                  <dd>{item.value}</dd>
-                </dl>
-              ))}
+              <div className={styles.infoList}>
+                {profileData.map((item, index) => (
+                  <dl className={styles.infoItem} key={index}>
+                    <dt>{item.txt}</dt>
+                    <dd>{item.value}</dd>
+                  </dl>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {awardsData.length > 0 && (
-          <div className={styles.infoBox}>
-            <p className={styles.title}><FcCloseUpMode />AWARDS</p>
+          {awardsData.length > 0 && (
+            <div className={styles.infoBox}>
+              <p className={styles.title}>
+                <FcCloseUpMode />
+                AWARDS
+              </p>
 
-            <div className={styles.infoList}>
-              {awardsData.map((item, index) => (
-                <dl className={styles.infoItem} key={index}>
-                  <dt>{item.date}</dt>
-                  <dd>{item.txt}</dd>
-                </dl>
-              ))}
+              <div className={styles.infoList}>
+                {awardsData.map((item, index) => (
+                  <dl className={styles.infoItem} key={index}>
+                    <dt>{item.date}</dt>
+                    <dd>{item.txt}</dd>
+                  </dl>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {activitiesData.length > 0 && (
-          <div className={styles.infoBox}>
-            <p className={styles.title}><FcSportsMode />ACTIVITIES</p>
+          {activitiesData.length > 0 && (
+            <div className={styles.infoBox}>
+              <p className={styles.title}>
+                <FcSportsMode />
+                ACTIVITIES
+              </p>
 
-            <div className={styles.infoList}>
-              {activitiesData.map((item, index) => (
-                <dl className={styles.infoItem} key={index}>
-                  <dt>{item.date}</dt>
-                  <dd>{item.txt}</dd>
-                </dl>
-              ))}
+              <div className={styles.infoList}>
+                {activitiesData.map((item, index) => (
+                  <dl className={styles.infoItem} key={index}>
+                    <dt>{item.date}</dt>
+                    <dd>{item.txt}</dd>
+                  </dl>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {stackData.length > 0 && (
-          <div className={styles.infoBox}>
-            <p className={styles.title}><FcSupport />TECH.STACK</p>
+          {stackData.length > 0 && (
+            <div className={styles.infoBox}>
+              <p className={styles.title}>
+                <FcSupport />
+                TECH.STACK
+              </p>
 
-            <div className={styles.stackWrap}>
-              {stackData.map((item, index) => (
-                <div key={index} className={styles.stack}>
-                  <span
-                    className={`${styles.stackIcon} ${styles[item.icon]}`}
-                    style={{
-                      background: `url(${process.env.ASSET_PREFIX}/stackIcon.svg) no-repeat center/auto 100%`,
-                    }}
-                  ></span>
-                  <span className={styles.txt}>{item.txt}</span>
-                </div>
-              ))}
+              <div className={styles.stackWrap}>
+                {stackData.map((item, index) => (
+                  <div key={index} className={styles.stack}>
+                    <span
+                      className={`${styles.stackIcon} ${styles[item.icon]}`}
+                      style={{
+                        background: `url(${process.env.ASSET_PREFIX}/stackIcon.svg) no-repeat center/auto 100%`,
+                      }}
+                    ></span>
+                    <span className={styles.txt}>{item.txt}</span>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {projectData.length > 0 && (
-          <div className={`${styles.infoBox} ${styles.half}`}>
-            <p className={styles.title}><FcFolder />PROJECT</p>
+          {projectData.length > 0 && (
+            <div className={styles.infoBox}>
+              <p className={styles.title}>
+                <FcFolder />
+                PROJECT
+              </p>
 
-            <div className={styles.infoList}>
-              {projectData.map((item, index) => (
-                <dl className={styles.infoItem} key={index}>
-                  <dt>{item.date}</dt>
-                  <dd>{item.txt}</dd>
-                </dl>
-              ))}
+              <div className={styles.infoList}>
+                {projectData.map((item, index) => (
+                  <dl className={styles.infoItem} key={index}>
+                    <dt>{item.date}</dt>
+                    <dd>{item.txt}</dd>
+                  </dl>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        className={styles.swiperContainer}
+      >
+        <SwiperSlide className={styles.swiperSlide}>
+          <div className={styles.item}>
+            {profileData.length > 0 && (
+              <div className={styles.infoBox}>
+                <p className={styles.title}>
+                  <FcBusinessContact />
+                  PROFILE
+                </p>
+
+                <div className={styles.infoList}>
+                  {profileData.map((item, index) => (
+                    <dl className={styles.infoItem} key={index}>
+                      <dt>{item.txt}</dt>
+                      <dd>{item.value}</dd>
+                    </dl>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.swiperSlide}>
+          <div className={styles.item}>
+            {awardsData.length > 0 && (
+              <div className={styles.infoBox}>
+                <p className={styles.title}>
+                  <FcCloseUpMode />
+                  AWARDS
+                </p>
+
+                <div className={styles.infoList}>
+                  {awardsData.map((item, index) => (
+                    <dl className={styles.infoItem} key={index}>
+                      <dt>{item.date}</dt>
+                      <dd>{item.txt}</dd>
+                    </dl>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.swiperSlide}>
+          <div className={styles.item}>
+            {activitiesData.length > 0 && (
+              <div className={styles.infoBox}>
+                <p className={styles.title}>
+                  <FcSportsMode />
+                  ACTIVITIES
+                </p>
+
+                <div className={styles.infoList}>
+                  {activitiesData.map((item, index) => (
+                    <dl className={styles.infoItem} key={index}>
+                      <dt>{item.date}</dt>
+                      <dd>{item.txt}</dd>
+                    </dl>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.swiperSlide}>
+          <div className={styles.item}>
+            {projectData.length > 0 && (
+              <div className={`${styles.infoBox} ${styles.half}`}>
+                <p className={styles.title}>
+                  <FcFolder />
+                  PROJECT
+                </p>
+
+                <div className={styles.infoList}>
+                  {projectData.map((item, index) => (
+                    <dl className={styles.infoItem} key={index}>
+                      <dt>{item.date}</dt>
+                      <dd>{item.txt}</dd>
+                    </dl>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -3,6 +3,8 @@ import styles from "./ProjectCard.module.scss";
 import { FcServices, FcBrokenLink, FcSms } from "react-icons/fc";
 
 const ProjectCard = ({ project }) => {
+  const [thumbShow, setThumbShow] = useState(false);
+
   return (
     <div className={styles.projectCard}>
       <p className={styles.projectName}>{project.projectName}</p>
@@ -15,14 +17,23 @@ const ProjectCard = ({ project }) => {
 
       <p className={styles.projectDescription}>{project.description}</p>
 
-      <div className={styles.flexBox}>
-        <div className={styles.thumb}>
+      <div className={`${styles.flexBox} ${thumbShow ? styles.isShow : ""}`}>
+        <div className={`${styles.thumb}`}>
           <div
             className={styles.thumbImg}
             style={{
               background: `url(${process.env.ASSET_PREFIX}/${project.thumb}) no-repeat center/cover`,
             }}
           ></div>
+
+          <button
+            onClick={() => setThumbShow(true)}
+            type="button"
+            title="자세히 보기"
+            className={styles.btnDetail}
+          >
+            자세히 보기
+          </button>
         </div>
 
         <div className={styles.info}>
